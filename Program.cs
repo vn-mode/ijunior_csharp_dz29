682e6f82f447ec5a08eb97239c665618ada58d64
VITALII NAGIBIN <98809212+vn-mode@users.noreply.github.com>
2023-02-28 01:46:21 +0300

    ijunior_csharp_dz29

--- Program.cs
+++ Program.cs
@@ -14,13 +14,14 @@ namespace vn_mode_csharp_dz29
             while (isOpen)
             {
                 Console.SetCursorPosition(0, 0);
-                EditeHealthbar(playerHealth, maxHealth);
+                DrawHealthbar(playerHealth, maxHealth);
+
                 Console.SetCursorPosition(0, 3);
                 Console.WriteLine("Доступные команды:");
                 Console.WriteLine("Ввести желаемый процент здоровья - 1");
                 Console.WriteLine("Выйти из программы - 2" + "");
-                Console.SetCursorPosition(0, 7);
 
+                Console.SetCursorPosition(0, 7);
                 Console.Write("Введите команду: ");
 
                 switch (Console.ReadLine())
@@ -28,7 +29,7 @@ namespace vn_mode_csharp_dz29
                     case "1":
                         Console.Write("Введите желаемый процент здоровья: ");
                         playerHealth = Convert.ToInt32(Console.ReadLine());
-                        EditeHealthbar(playerHealth, maxHealth);
+                        DrawHealthbar(playerHealth, maxHealth);
                         break;
 
                     case "2":
@@ -42,7 +43,7 @@ namespace vn_mode_csharp_dz29
             }
         }
 
-        static void EditeHealthbar(int playerHealth, int maxHealth, char symbolHealth = '#', char symbolEmptyHealth = '_')
+        static void DrawHealthbar(int playerHealth, int maxHealth, char symbolHealth = '#', char symbolEmptyHealth = '_')
         {
             string bar = "";
 
